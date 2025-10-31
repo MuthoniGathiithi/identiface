674cdf98c47d64582b4a3b11d820e9d78985aecc
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-10-31 17:39:35 +0300

    add views to render the landing page

--- face_recognition/views.py
+++ face_recognition/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'face_recognition/landing_page.html')
+
+def signup(request):
+    return render(request, 'face_recognition/signup.html')
+
+def login(request):
+    return render(request, 'face_recognition/login.html')
+
+    
