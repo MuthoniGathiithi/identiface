bd18af9309631f0b5b9e040addf674d61f5f4304
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-10-31 17:38:50 +0300

    add path signup ,login  in views in urls patterns

--- face_recognition/urls.py
+++ face_recognition/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login')
 ]
